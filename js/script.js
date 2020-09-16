577746c361fe0a9f8d6ac383e62037da72c1176b
kostya <matvienkoigor956@gmail.com>
2020-09-16 18:01:16 +0300

    finished

--- js/script.js
+++ js/script.js
@@ -11,16 +11,32 @@ $(document).ready(function() {
         pauseOnHover: false,
         pauseOnFocus: false,
     });
-    $('.algorithm_items_slider').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
-        speed: 800,
-        // vertical: true,
-        // arrows: true,
-        easing: 'linear',
-        // pauseOnHover: false,
-        // pauseOnFocus: false,
+
+
+
+
+    if ($(window).width() >= 1200) {
+        $('.algorithm_items_slider').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            speed: 800,
+            easing: 'linear',
+            // arrows: false,
+            responsive: [
+
+                {
+                    breakpoint: 1430,
+                    settings: {
+                        arrows: false
+                    }
+                }
+            ]
+        });
+    }
+
+    // ------------------------------------
+    $('.button').click(() => {
+        return false
     });
+    // ------------------------------------   
 });
