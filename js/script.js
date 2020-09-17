2d081c66c9618707ff1f24a34bf3f15aa3f5e238
kostya <matvienkoigor956@gmail.com>
2020-09-17 12:56:55 +0300

    added settings for slick slider

--- js/script.js
+++ js/script.js
@@ -1,38 +1,5 @@
 $(document).ready(function() {
-    $('.specialization_autoplay').slick({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 800,
-        speed: 1500,
-        vertical: true,
-        arrows: false,
-        easing: 'easy',
-        pauseOnHover: false,
-        pauseOnFocus: false,
-    });
-
-    if ($(window).width() >= 1200) {
-        $('.algorithm_items_slider').slick({
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            speed: 800,
-            easing: 'linear',
-            // arrows: false,
-            responsive: [
-
-                {
-                    breakpoint: 1430,
-                    settings: {
-                        arrows: false
-                    }
-                }
-            ]
-        });
-
-    }
-    // ------------------------------------
-
+    // ------------------------------------ hide/show text 
     if ($(window).width() <= 576) {
         $(".content").hide();
         $(".show_hide").on("click", function() {
@@ -42,14 +9,14 @@ $(document).ready(function() {
         });
 
     }
-    // ------------------------------------
+    // ------------------------------------ scroll to id
     $(".nav_link").on("click", function(event) {
         event.preventDefault();
         var id = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({ scrollTop: top }, 500);
     });
-    // ------------------------------------
+    // ------------------------------------ reload page disable
     $('.button').click(() => {
         return false
     });
